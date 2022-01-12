9e165ebc9e4512eeba801bb89bcb76e1436c150b
Gabor Szasz <skywallalp@gmail.com>
2022-01-12 20:19:02 +0100

    additions implemented

--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -11,16 +11,16 @@ export class FilterPipe implements PipeTransform {
    * @param key {string} - az objektumkulcs, amely alapján szűr
    * @returns {any[]} - a kifejezés alapján szűrt tömb
    */
-  transform(value: any[], phrase: string, key: string = ''): any {
+  transform(value: any[] , phrase: string, key: string = ''): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
+    // return value;
 
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb, vagy nincs megadva a phrase vagy a key,
      * térj vissza a value változóval.
      */
-
+    if(!Array.isArray(value) || !phrase || !key) { return value}
 
 
     /**
@@ -30,7 +30,10 @@ export class FilterPipe implements PipeTransform {
      * 2. A visszatérési érték true, ha valahol szerepel benne a phrase.
      * TIPP: az összehasonlítás előtt a két értéket alakítsd kisbetűsre.
      */
-
+     return value.filter( item => {
+      const data = String(item[key]).toLowerCase();
+      return data.includes(phrase.toLowerCase());
+    });
 
   }
 
